4d7e9133fdff1a20e490c71bd3b134a06d5e3f7b
Abel <abelguevaralanda@yahoo.es>
2023-02-06 21:09:31 +0100

    add info and gentleman component

---
+++ src/components/Gentleman/Gentleman.tsx
@@ -0,0 +1,46 @@
+import { FC } from "react";
+import Gentlemen from "../../typesgentleman";
+
+interface GentlemenData {
+  gentlemen: Gentlemen;
+}
+
+const Card: FC<GentlemenData> = ({ gentlemen }) => {
+  return (
+    <main className="main">
+      <ul className="gentlemen">
+        <li className="gentleman">
+          <div className="gentleman__avatar-container">
+            <img
+              className="gentleman__avatar"
+              src={`img/${gentlemen.picture}`}
+              alt="The Fary pointing at you"
+            />
+            <span className="gentleman__initial">{gentlemen.name[0]}</span>
+          </div>
+          <div className="gentleman__data-container">
+            <h2 className="gentleman__name">{gentlemen.name}</h2>
+            <ul className="gentleman__data-list">
+              <li className="gentleman__data">
+                <span className="gentleman__data-label">Profession:</span>
+                {gentlemen.profession}
+              </li>
+              <li className="gentleman__data">
+                <span className="gentleman__data-label">Status:</span>{" "}
+                {gentlemen.status}
+              </li>
+              <li className="gentleman__data">
+                <span className="gentleman__data-label">Twitter:</span>{" "}
+                {gentlemen.twitter}
+              </li>
+            </ul>
+          </div>
+          <i className="icon gentleman__icon fas fa-check"></i>
+          <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+        </li>
+      </ul>
+    </main>
+  );
+};
+
+export default Card;
